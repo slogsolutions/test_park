7abc13ad106ce33649f577a96d8f3a2440326af6
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-09-23 16:40:27 +0530

    v6 added Notification in admin panel

--- backend/utils/firebase.js
+++ backend/utils/firebase.js
@@ -1,30 +1,43 @@
-// import admin from"firebase-admin";
+// backend/config/firebaseAdmin.js
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-// import serviceAccount from "../utils/firebaseAdminSDK.json";
+dotenv.config();
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+// Load service account from env (safer than JSON file committed in repo)
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // fix line breaks
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
-// -----------------------------------------
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export default admin;
+
+
 
 // import admin from "firebase-admin";
-// import serviceAccount from "../utils/firebaseAdminSDK.json" assert { type: "json" };
+// import fs from "fs";
+
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(new URL("./firebaseAdminSDK.json", import.meta.url))
+// );
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // });
 
-// ---------------
-import admin from "firebase-admin";
-import fs from "fs";
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./firebaseAdminSDK.json", import.meta.url))
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export default admin;
+// export default admin;
