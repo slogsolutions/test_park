1d7efc6ff7132b9e84fc31f98494b0e1ecdd3d76
slogsolutions <slogsolutions.it@gmail.com>
2025-09-27 14:04:33 +0530

    Update Login.tsx
    FOR TESTING ADDED DEV IN welcome Back

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -47,7 +47,7 @@ export default function Login() {
           </div>
         </div>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Welcome Back
+          Welcome Back [ANDROID DEV]
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Sign in to access your account
